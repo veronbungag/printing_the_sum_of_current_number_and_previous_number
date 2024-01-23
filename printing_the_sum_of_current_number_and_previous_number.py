12ee6693583ea11b4cc445180ad91b5c3814ae6e
veronbungag <rveron.bungag@gmail.com>
2024-01-23 21:48:49 +0800

    for loop 1-10

--- printing_the_sum_of_current_number_and_previous_number.py
+++ printing_the_sum_of_current_number_and_previous_number.py
@@ -3,6 +3,8 @@ print("Printing current and previous number and their sum in a range(10)")
 #modify the previous number as 0
 previous_number = 0
 #loop from 1 to 10
+for i in range(1, 10):
+    sum = previous_number + i
 #print the current number, previous number, and the sum
 #modify previous number
 #set it to i
