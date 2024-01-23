b2ba2ced16d90376330883333862d72852d2a1dc
veronbungag <rveron.bungag@gmail.com>
2024-01-23 21:45:57 +0800

    pseudocode

---
+++ printing_the_sum_of_current_number_and_previous_number.py
@@ -0,0 +1,6 @@
+#print the current and previous number in a range 1-10
+#modify the previous number as 0
+#loop from 1 to 10
+#print the current number, previous number, and the sum
+#modify previous number
+#set it to i
